26aff1fccf0f708ef2c989d7a81ade597c7bd12e
MohdFaizU <131484663+MohdFaizU@users.noreply.github.com>
2024-06-05 10:04:14 -0400

    Update LeetCode 27.py
    Added description for the problem and solution.

--- LeetCode 27.py
+++ LeetCode 27.py
@@ -1,3 +1,11 @@
+# https://leetcode.com/problems/remove-element/
+
+'''
+Problem: Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
+
+Solution: We can solve the problem by using two pointers. We can iterate through the array with the right pointer.
+If the element at the right pointer is equal to val, we set the element at the right pointer to 0. Otherwise, we copy the element at the right pointer to the left pointer and increment the left pointer.
+'''
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         l = 0
