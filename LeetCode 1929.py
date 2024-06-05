a74857b14dd6e32c5b4494a4b1962d426ff3fa8f
MohdFaizU <131484663+MohdFaizU@users.noreply.github.com>
2024-06-05 10:01:32 -0400

    Update LeetCode 1929.py
    Added description for problem and solution.

--- LeetCode 1929.py
+++ LeetCode 1929.py
@@ -1,3 +1,10 @@
+# https://leetcode.com/problems/concatenation-of-array/
+
+'''
+Problem: Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+
+Solution: We can solve the problem by iterating through the array twice. We can append the elements of the array to the answer array twice.
+'''  
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
         ans = []
